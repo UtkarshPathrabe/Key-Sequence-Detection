10e0dbb3325803896d64d0491a8b3764fc64313c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-01-16 15:54:22 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const pressed = [];
+const secretCode = 'utkarsh';
+window.addEventListener('keyup', (e) => {
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        cornify_add();
+    }
+});
+window.addEventListener('load', () => {
+    cornify_count = 0;
+    cornify_setcookie('cornify', cornify_count + '', 1000);
+});
